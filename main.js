b86b13e66907e42dce1845a65d45d70a66750367
jo4nny42 <tagehodaniels@gmail.com>
2023-10-12 01:38:17 -0400

    Seperate

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let index = 1;
+let index2 = 1;
 showSlides(index);
+showSlides2(index2);
 
 
 function nextSlide(n){
@@ -9,6 +11,16 @@ function nextSlide(n){
 
 function currentSlide(n){
     showSlides(index=n);
+}
+
+
+function nextSlide2(n){
+  showSlides2(index2+=n);
+}
+
+
+function currentSlide2(n){
+  showSlides2(index2=n);
 }
 
 function showSlides(n){
@@ -28,4 +40,23 @@ function showSlides(n){
   slides[index-1].style.display = " block";
   dots[index-1].className += " active";
   captionText.innerHTML = dots[index-1].alt;
+}
+
+function showSlides2(n){
+  let i;
+  let slides = document.getElementsByClassName("contentBorder2");
+  let dots = document.getElementsByClassName("Demo");
+  let captionText = document.getElementById("caption2");
+  if (n > slides.length) {index2 = 1}
+  if (n < 1) {index2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[index2-1].style.display = " block";
+  dots[index2-1].className += " active";
+  captionText.innerHTML = dots[index2-1].alt;
 }
