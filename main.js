fe2e7e4dcd097a14e7228a3d10b78a7dd7f43256
jo4nny42 <tagehodaniels@gmail.com>
2023-10-12 23:04:06 -0400

    end

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let index,index2,index3,index4 = 1;
+let index,index2,index3,index4,index5,index6 = 1;
 showSlides(index);
 showSlides2(index2);
 showSlides3(index3);
@@ -34,6 +34,14 @@ function nextSlide4(n){
 
 function currentSlide4(n){
   showSlides4(index4=n);
+}
+
+function currentSlide5(n){
+  showSlides5(index5=n);
+}
+
+function currentSlide6(n){
+  showSlides6(index6=n);
 }
 
 function showSlides(n){
@@ -110,4 +118,42 @@ function showSlides4(n){
   slides[index4-1].style.display = " block";
   dots[index4-1].className += " active";
   captionText.innerHTML = dots[index4-1].alt;
+}
+
+function showSlides5(n){
+  let i;
+  let slides = document.getElementsByClassName("contentBorder5");
+  let dots = document.getElementsByClassName("time5");
+  let captionText = document.getElementById("caption5");
+  if (n > slides.length) {index5 = 1}
+  if (n < 1) {index5 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[index5-1].style.display = " block";
+  dots[index5-1].className += " active";
+  captionText.innerHTML = dots[index5-1].alt;
+}
+
+function showSlides6(n){
+  let i;
+  let slides = document.getElementsByClassName("contentBorder6");
+  let dots = document.getElementsByClassName("time6");
+  let captionText = document.getElementById("caption6");
+  if (n > slides.length) {index6 = 1}
+  if (n < 1) {index6 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[index6-1].style.display = " block";
+  dots[index6-1].className += " active";
+  captionText.innerHTML = dots[index6-1].alt;
 }
